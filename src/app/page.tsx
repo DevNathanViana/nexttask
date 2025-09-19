9c5ef8d73479bddaac01b9b620b94e75a6358640
Nathan Viana <oliveiranathan700@gmail.com>
2025-09-18 21:42:12 -0300

    Commit inicial - Tela de login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,75 @@
-import Image from "next/image";
+import { FaGithub, FaGoogle } from "react-icons/fa";
+
+export const metadata = {
+  title: "Login - NextTask",
+  description: "Faça login no NextTask",
+  icons: {
+    icon: "/next_task_logo.png",
+    shortcut: "/next_task_logo.png",
+  },
+};
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <div className="w-screen h-screen bg-white-200 flex p-4">
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+      <div className="w-1/2 h-full border-1 border-gray-200 rounded p-10 flex flex-col justify-center items-center">
+        <div className="w-1/2 h-1/2 flex flex-col gap-7 flex flex-col justify-center items-center">
+
+          <h1 className="text-black-300 text-5xl pb-3">Welcome</h1>
+
+          <div className="flex flex-col w-full gap-4">
+            <button className="w-full h-12 border-2 border-gray-300 rounded cursor-pointer flex justify-center items-center gap-4"><FaGoogle size={24} />Sign in with Google</button>
+            <button className="w-full h-12 border-2 border-gray-300 rounded cursor-pointer flex justify-center items-center gap-4"><FaGithub size={24} />Sign in with Github</button>
+          </div>
+
+          <div className="flex items-center w-full py-5">
+            <div className="flex-grow border-t border-gray-300"></div>
+            <span className="px-3 text-gray-500">or</span>
+            <div className="flex-grow border-t border-gray-300"></div>
+          </div>
+
+          <div className="flex flex-col w-full gap-6">
+            <div>
+              <label htmlFor="email">Email</label>
+              <input
+                placeholder="example@gmail.com"
+                id="email"
+                className="w-full h-12 border-2 border-gray-300 rounded focus:border-gray-400 focus:ring-2 focus:ring-gray-200 outline-none p-2"
+                type="text"
+              />
+            </div>
+
+            <div>
+              <label htmlFor="senha">Password</label>
+              <input
+                id="senha"
+                placeholder=""
+                className="w-full h-12 border-2 border-gray-300 rounded focus:border-gray-400 focus:ring-2 focus:ring-gray-200 outline-none p-2"
+                type="password"
+              />
+            </div>
+
+            <div className="pt-4">
+              <button className="w-full h-12 bg-[linear-gradient(0deg,rgba(26,145,86,1)_0%,rgba(0,191,73,1)_100%)] active:scale-95 rounded cursor-pointer text-white font-semibold transition-all duration-300 ">
+                Login
+              </button>
+            </div>
+
+            <div className="pt-2 w-full flex justify-center items-center">
+              <p>Forgot password?</p>
+            </div>
+          </div>
+
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
+
+      <div
+        className="w-1/2 h-full border border-gray-200 rounded bg-cover bg-center"
+        style={{ backgroundImage: "url('/fundo_login.jpg')" }}
+      >
+      </div>
+
     </div>
   );
 }
